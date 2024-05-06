3ee3466c0a624f456c545fb772f45c2df5a30665
AmeeYuki <yukinooame@gmail.com>
2024-05-06 13:45:34 +0700

    update layout

---
+++ Front-end/Jewelry Sales System At The Store/src/layout/MainLayout.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UserOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import {
+  Layout,
+  Menu,
+  Button,
+  theme,
+  Avatar,
+  Badge,
+  Dropdown,
+  Space,
+} from "antd";
+import useSider from "@/hooks/useSider";
+import { Link, useLocation } from "react-router-dom";
+import HeaderComponent from "../components/header/HeaderComponent";
+
+const { Header, Sider, Content } = Layout;
+
+const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  // const dispatcher = useAppDispatch();
+  const {
+    token: { colorBgContainer, borderRadiusLG, ...other },
+  } = theme.useToken();
+  const location = useLocation();
+
+  const siderList = useSider();
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+      <Layout
+        style={{
+          height: "100vh",
+        }}
+      >
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div
+            style={{
+              height: "100%",
+              padding: "16px",
+              flex: 1,
+            }}
+          >
+            <Menu
+              style={{
+                borderRadius: borderRadiusLG,
+                height: "100%",
+                boxShadow: other.boxShadow,
+                background: other.colorBgBlur,
+                color: other.colorTextLightSolid,
+              }}
+              theme="light"
+              mode="inline"
+              selectedKeys={[location.pathname.substring(1)]}
+              items={[
+                ...siderList.map((item) => {
+                  return {
+                    ...item,
+                    key: item.href,
+                    label: <Link to={item.href}>{item.label}</Link>,
+                  };
+                }),
+              ]}
+            />
+          </div>
+        </Sider>
+        <Layout>
+          <Content
+            style={{
+              display: "flex",
+              margin: "16px 16px",
+              padding: 12,
+              minHeight: 280,
+              background: other.colorBorderSecondary,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Outlet />
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
+};
+
+export default MainLayout;
