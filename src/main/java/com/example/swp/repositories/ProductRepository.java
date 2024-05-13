f5c93932beb405fd03509c0ec067541c0e8443d2
AmeeYuki <124485897+AmeeYuki@users.noreply.github.com>
2024-05-13 13:17:56 +0700

    Revert "Sondev"

--- src/main/java/com/example/swp/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.swp.repositories;
-
-import com.example.swp.entities.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Products,Long> {
-}
--- src/main/java/com/example/swp/services/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.example.swp.services;
-
-import com.example.swp.dtos.ProductDTO;
-import com.example.swp.entities.Products;
-import com.example.swp.exceptions.DataNotFoundException;
-
-public interface IProductService {
-    Products createProduct(ProductDTO productDTO) throws DataNotFoundException;
-}
