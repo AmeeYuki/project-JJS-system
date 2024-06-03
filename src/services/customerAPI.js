73cfda741d539c7f3e20cf8ddff086802232c4bd
AmeeYuki <yukinooame@gmail.com>
2024-06-03 13:07:03 +0700

    merge and update api

---
+++ src/services/customerAPI.js
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_URL_BE } from "../config";
+import { selectToken } from "../slices/auth.slice";
+
+export const customerAPI = createApi({
+  reducerPath: "customerManagement",
+  tagTypes: ["CustomerList"],
+  baseQuery: fetchBaseQuery({
+    baseUrl: API_URL_BE,
+    prepareHeaders: (headers, { getState }) => {
+      const token = selectToken(getState()); // Retrieve token from Redux state using selectToken selector
+      if (token) {
+        headers.append("Authorization", `Bearer ${token}`);
+      }
+      headers.append("Content-Type", "application/json");
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getAllCustomer: builder.query({
+      query: () => `users/get_all_users?page=0&limit=10000 `,
+      // `providesTags` determines which 'tag' is attached to the
+      // cached data returned by the query.
+      providesTags: (result) =>
+        result
+          ? result.customer.map(({ id }) => ({ type: "CustomerList", id }))
+          : [{ type: "CustomerList", id: " LIST " }],
+    }),
+  }),
+});
+
+export const { useGetAllCustomerQuery } = customerAPI;
