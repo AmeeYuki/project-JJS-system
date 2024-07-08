b59f4ca32945251220a7f45a4284401ffad1a33c
hhq160325 <142112839+hhq160325@users.noreply.github.com>
2024-07-08 10:17:57 +0700

    fix api
    usedeletebyid

--- src/services/promotionAPI.js
+++ src/services/promotionAPI.js
@@ -1,68 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { API_URL_BE } from "../config";
-// import { selectToken } from "../slices/auth.slice";
-
-// export const promotionAPI = createApi({
-//   reducerPath: "promotionManagement",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: API_URL_BE,
-//     prepareHeaders: (headers, { getState }) => {
-//       const token = selectToken(getState());
-//       if (token) {
-//         headers.set("Authorization", `Bearer ${token}`);
-//       }
-//       headers.set("Content-Type", "application/json");
-//       return headers;
-//     },
-//   }),
-//   tagTypes: ["PromotionList"],
-//   endpoints: (builder) => ({
-//     getAllPromotions: builder.query({
-//       query: () => "promotions/get_all_promotions",
-//       providesTags: (result) =>
-//         result
-//           ? result.map(({ id }) => ({ type: "PromotionList", id }))
-//           : [{ type: "PromotionList", id: "LIST" }],
-//     }),
-//     addPromotion: builder.mutation({
-//       query: (newPromotion) => ({
-//         url: "promotions/create",
-//         method: "POST",
-//         body: newPromotion,
-//       }),
-//       invalidatesTags: [{ type: "PromotionList", id: "LIST" }],
-//     }),
-//     updatePromotion: builder.mutation({
-//       query: ({ id, ...updatedPromotion }) => ({
-//         url: `promotions/update/${id}`,
-//         method: "PUT",
-//         body: updatedPromotion,
-//       }),
-//       invalidatesTags: ({ id }) => [{ type: "PromotionList", id }],
-//     }),
-//     deletePromotion: builder.mutation({
-//       query: (id) => ({
-//         url: `promotions/delete/${id}`,
-//         method: "DELETE",
-//       }),
-//       invalidatesTags: ({ id }) => [{ type: "PromotionList", id }],
-//     }),
-//     deleteExpiredPromotions: builder.mutation({
-//       query: () => ({
-//         url: "promotions/delete_expired_promotions",
-//         method: "DELETE",
-//       }),
-//     }),
-//   }),
-// });
-
-// export const {
-//   useGetAllPromotionsQuery,
-//   useAddPromotionMutation,
-//   useUpdatePromotionMutation,
-//   useDeletePromotionMutation,
-//   useDeleteExpiredPromotionsMutation,
-// } = promotionAPI;
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { API_URL_BE } from "../config";
 import { selectToken } from "../slices/auth.slice";
@@ -105,7 +40,7 @@ export const promotionAPI = createApi({
         method: "POST",
         body: newPromotion,
       }),
-      invalidatesTags: [{ type: "PromotionList", id: "LIST" }],
+      invalidatesTags: ["PromotionList"],
     }),
     deletePromotion: builder.mutation({
       query: (id) => ({
@@ -120,6 +55,7 @@ export const promotionAPI = createApi({
         url: "promotions/delete_expired_promotions",
         method: "DELETE",
       }),
+    }),
     usePromotion: builder.mutation({
       query: (code) => ({
         url: `promotions/use/${code}`,
